ccff5e1fc10bd12c6886d003e9caaaca80f3a5b5
Ramneek Singh <singhrramneek@gmail.com>
2024-09-17 12:39:59 +0530

    implemented  dotenv in backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://siddarthvedantu:siddarthsingh@cluster0.zow3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { // Change the URI to match your database
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('MongoDB connected...');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   }
 };
 
