dc89f1249efde45da0d92afaf7c611be39a91428
Ramneek Singh <singhrramneek@gmail.com>
2024-09-17 12:40:55 +0530

    implemented simple auth code (TO BE CHANGED) and implemented CORS

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/database'); 
 const evidenceRoutes = require('./routes/Evidence');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 2000; // Port can be set using environment variables
-
 // Connect to MongoDB
 connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors())
 
+app.post("/api/auth",async (req, res) => {
+  console.log(req.body.address);
+  if (req.body.address == "0x53dd4A0f08D0519AAEACeDBc607FD788b285FdBf") {
+    res.send({id:123,authorized:true,type:"police"})
+  }
+  else {
+    res.send({authorized:false})
+  }
+})
 // Define routes
 app.use('/api/evidence', evidenceRoutes);
 
