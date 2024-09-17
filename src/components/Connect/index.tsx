e1e803ef7ad181f7c736a418edba5b305e6b52b3
Ramneek Singh <singhrramneek@gmail.com>
2024-09-17 17:36:05 +0530

    connect component final

--- src/components/Connect/index.tsx
+++ src/components/Connect/index.tsx
@@ -7,38 +7,44 @@ import { checkAuth, printNew } from "../../api/auth";
 import { useUserStore } from "../../store";
 import { UserStore } from "../../store/globalSlices";
 
-export default function Connect() {
-	const [res, setRes] = useState<Partial<UserStore>>(); // Directly store the response data
-	const { address } = useAccount();
-	const setUser = useUserStore(state=>state.setUser)
 
+export default function Connect() {
+	const {setUser,authorized} = useUserStore();
+	const { address } = useAccount();
+	
 	const fetchAuth = async (address: string) => {
 		try {
 			const response = await checkAuth(address);
-			setRes(response); // Set the response directly
+			setUser(response);
 		} catch (error) {
 			console.error("Error in authorization:", error);
 		}
 	};
-
+	const AuthMessage = ()=>{
+		if (address){
+			if (authorized) {
+                return <p className="font-bold text-emerald-500 hover:text-teal-500 transition-colors duration-200 cursor-alias">Authorized</p>;
+            } else {
+                return <p className="font-bold text-red-600 hover:text-red-500 transition-colors duration-200 cursor-not-allowed">Not authorized</p>;
+            }
+		}
+		else {
+            return <p className="text-gray-300 hover:text-gray-500 transition-colors duration-200">Connect to access Dashboard</p>;
+        }
+	}
 	useEffect(() => {
 		if (address) {
 			fetchAuth(address);
 		}
-	}, [address]);
-
-	useEffect(() => {
-		if (res) {
-			// console.log(res);
-			setUser(res);
+		else {
+			fetchAuth("");
 		}
-	}, [res]);
-
+	}, [address]);
 	return (
 		<>
-			<div className="mt-10">
-				{JSON.stringify(res)} {/* Display the response */}
+			<div className="mt-10 mb-5 flex items-center gap-3">
 				<ConnectButton />
+				<AuthMessage/>
 			</div>
 		</>
 	);
