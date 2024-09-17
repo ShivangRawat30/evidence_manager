7a4efbbbec26e20fc002e2e745f828abb613dc27
Ramneek Singh <singhrramneek@gmail.com>
2024-09-17 14:52:47 +0530

    fixed app directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,17 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-
 import { config } from "../wagmi";
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Background from "../components/Background";
-import { useUserStore } from "../store";
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const id = useUserStore(state=>state.setUser);
-	id({authorized:true,id:"new",address:"SADA",type:"police"});
-	const newUser = useUserStore(state=>state.id);
-	console.log(newUser);
 	return (
 		<WagmiProvider config={config}>
 			<QueryClientProvider client={client}>
@@ -28,14 +21,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 						accentColorForeground: "#fff",
 						borderRadius: "large",
 						fontStack: "system",
-						overlayBlur: "small"
+						overlayBlur: "small",
 					})}>
 					<ChakraProvider>
 						<div className="font-sans">
-						<Header />
-						<Background>
-							<Component {...pageProps} />
-						</Background>
+							<Header />
+							<Background>
+								<Component {...pageProps} />
+							</Background>
 						</div>
 					</ChakraProvider>
 				</RainbowKitProvider>
