c480053de5b1a1159fff279870491915a7a033df
Ramneek Singh <singhrramneek@gmail.com>
2024-09-19 21:39:54 +0530

    added public key to Dashboard

--- src/pages/dashboard/Window/index.tsx
+++ src/pages/dashboard/Window/index.tsx
@@ -1,11 +1,13 @@
-export default function Page({user}) {
+import {  UserStore } from "../../../store/globalSlices";
+export default function Page({ user }: { user: UserStore }) {
     return (
         <div className=" col-span-9 grid grid-rows-12">
-                <div className="text-[200%] font-black">
-                    {user.details.courtName||user.details.policeStationName}
-                </div>
-                <div className="bg-emerald-900/5 row-span-11 rounded-[11px]"></div>
-
+            <div className="text-[200%] font-black">
+                {user.details.courtName || user.details.policeStationName}
             </div>
+            <div className="bg-emerald-900/5 row-span-11 rounded-[11px]">
+                {user.details.publicKey}
+            </div>
+        </div>
     );
 }
